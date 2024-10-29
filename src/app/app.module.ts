4af76994fd18c50025742c6594311aa1efc1d35d
Sandro Roberto Ventura <sandro.ventura@linedata.com.br>
2024-10-29 13:37:24 -0300

    Implementado componentes de navbar e footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ExemploComponenteComponent } from './components/exemplo-componente/exemplo-componente.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExemploComponenteComponent
+    ExemploComponenteComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
